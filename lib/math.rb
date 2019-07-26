bad0128be7acfd3819e340c87e21868b0f256252
jharell chambers <jharell.chambers@selu.edu>
2019-07-26 01:40:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,6 @@
 def addition(num1, num2)
-
+ add = num1 + num2
+ return addition
 end
 
 def subtraction(num1, num2)
@@ -21,3 +22,11 @@ end
 def square_root(num)
 
 end
+
+
+addition (14, 56)
+subtraction (687. 300)
+multiplication (10, 20)
+division (2000, 80)
+modulo (56, 7)
+square_root (144)
