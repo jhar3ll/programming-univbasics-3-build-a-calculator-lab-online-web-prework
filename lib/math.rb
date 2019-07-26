3293f25aa7c33a9a2ede0fc45009c3901a8aa532
jharell chambers <jharell.chambers@selu.edu>
2019-07-26 02:00:53 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,32 @@
 def addition(num1, num2)
-
+ sum = num1 + num2
 end
 
 def subtraction(num1, num2)
-
+ difference = (num1 - num2)
 end
 
 def division(num1, num2)
-
+ quotient = (num1 / num2)
 end
 
 def multiplication(num1, num2)
-
+ product = (num1 * num2)
 end
 
 def modulo(num1, num2)
-
+ remainder = (num1 % num2)
 end
 
 def square_root(num)
+ Math.sqrt(num).round 
+ end
+ 
+ #used .round because I wanted to return an integer rather than                    a float :)
 
-end
+p addition(14,56)
+p subtraction(687,300)
+p multiplication(10,20)
+p division(2000,80)
+p modulo(58,7)
+p square_root(144)
