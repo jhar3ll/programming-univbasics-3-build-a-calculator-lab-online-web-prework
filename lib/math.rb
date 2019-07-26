a1e77144752931986d1a324ed481d219c89bc0c3
jharell chambers <jharell.chambers@selu.edu>
2019-07-26 01:45:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -23,9 +23,9 @@ def square_root(num)
 end
 
 
-p addition (14,56)
-p subtraction (687.300)
-p multiplication (10,20)
-p division (2000,80)
-p modulo (56,7)
-p square_root (144)
+p addition(14,56)
+p subtraction(687.300)
+p multiplication(10,20)
+p division(2000,80)
+p modulo(56,7)
+p square_root(144)
